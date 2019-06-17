64e2486f9e44819bd09a483466e48d5bc16361a7
Justin German <justin.german23@gmail.com>
2019-06-17 09:52:07 -0500

    schema and seeds updated

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Umami Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Meat Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", true);
