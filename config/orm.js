49b8c2afd777ab001cc93f1f85682d8d6db37528
Justin German <justin.german23@gmail.com>
2019-06-17 14:03:49 -0500

    created orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,32 @@
 var connection = require("../config/connection.js");
+
+
+var orm = {
+    selectAll: function(tableInput, callBack){
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result){
+            if (err){
+                throw err;
+            }
+            callBack(result);
+        });
+    },
+    insertOne: function(tableInput, colName, value, callBack){
+        value = connection.escape(value);
+        var queryString = "INSERY INTO " + tableInput + "(" + colName + ") VALUES (" + value + ");";
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            callBack(result);
+        });
+    },
+    updateOne: function (tableInput, colName, value, idColName, id, callBack){
+        var queryString = "UPDATE " + tableInput + " SET " + colName + " = " + value + " WHERE " + idColName + " = " + id + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            callBack(result);
+        });
+    }   
+}
+    
+
+module.exports = orm;
