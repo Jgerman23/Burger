0952bc1da71f3b9e94851d5bac4bdcd36ba8b6e2
Justin German <justin.german23@gmail.com>
2019-06-17 19:42:24 -0500

    made orm func calls & routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,26 @@
 var express = require("express");
-
+var router = express.Router();
 var burger = require("../models/burger.js");
+
+router.get("/", function(request, response){
+    burger.all(function(data){
+        var info = {
+            burgers: data 
+        };
+        response.render("index", info);
+    });
+});
+
+router.post("api/burgers", function(request, response){
+    burger.add(request.body.burger_name, function(result){
+        response.json({id: result.insertId});
+    });
+});
+
+router.put("api/burgers/:id", function(request, respnse){
+    burger.update(request.params.id, function(result){
+        response.json(result);
+    });
+});
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,21 @@
 var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(callBack){
+        orm.selectAll("burgers", function(response){
+            callBack(response);
+        });
+    },
+    add: function(value, callBack){
+        orm.insertOne("burgers", "burger_name", value, function(response){
+            callBack(response);
+        });
+    },
+    update: function(eatenId, callBack){
+        orm.updateOne("burgers", "devoured", 1, "id", eatenId, function(response){
+            callBack(response);
+        });
+    }
+};
+
+module.exports = burger;
